bbf71c4a7b7586ce3f287f412b8665bb17d08dc6
elegantman1 <60242303+elegantman1@users.noreply.github.com>
2020-01-23 14:40:59 -0800

    Created using Popcode: https://popcode.org

---
+++ script.js
@@ -0,0 +1,21 @@
+$("img.math").click(function() {
+    $(".Math").append("<br>Inverse functions ");
+});
+
+$("img.coding").click(function() {
+    $(".coding").append("<br>HTML! CSS! jQuery! ");
+});
+
+$("img.physics").click(function() {
+    $(".physics").append("<br>Newton's law! ");
+});
+
+$("img.english").click(function() {
+    $(".english").append("<br>Poem! ");
+});
+$("img.history").click(function() {
+    $(".history").append("<br>Native American!");
+});
+$("img.college").click(function(){
+    $(".college").append("<br>college!");
+});
